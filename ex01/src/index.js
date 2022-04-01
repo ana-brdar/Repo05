adab2db2aaea32b59c7aba3f1264f9a884963fd8
ana-brdar <brdar.ana@yahoo.com>
2022-04-01 22:42:05 +0200

    Added my personal information

---
+++ ex01/src/index.js
@@ -0,0 +1,15 @@
+function main() {
+    // only change code below this line
+    var myFirstName = "Ana";
+    var myLastName = "Brdar";
+    var myHomeTown = "Beograd";
+    // only change code above this line
+    return {
+      myFirstName,
+      myLastName,
+      myHomeTown,
+    };
+  }
+  
+  console.log(main());
+  module.exports = main;
