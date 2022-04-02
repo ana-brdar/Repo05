156b32cdb7a29af18964e2fcdfc3b1f71201d379
ana-brdar <brdar.ana@yahoo.com>
2022-04-02 02:19:25 +0200

    Word Blanks

---
+++ ex15/src/index.js
@@ -0,0 +1,12 @@
+function main() {
+    // Only change code below this code
+    var myNoun = "dog";
+    var myVerb = "ran";
+    var myAdjective = "big";
+    var myAdverb = "quickly";
+    var wordBlanks = "My " + myNoun + " was a " + myAdjective + " golden retriever and he " + myVerb + " very " + myAdverb + ".";
+    // Only change code above this code
+    return wordBlanks; 
+}
+console.log(main());
+module.exports = main;
