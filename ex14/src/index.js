658fe61520b895da67b5b98c5c1de0b80793102e
ana-brdar <brdar.ana@yahoo.com>
2022-04-02 02:12:34 +0200

    Use bracket notation to find the nth-to-last character in a string

---
+++ ex14/src/index.js
@@ -0,0 +1,9 @@
+function main() {
+    // only change code below this code
+    lastName = "Bezos";
+    var thirdToLastLetterOfLastName = lastName[lastName.length - 3];
+    // only change code above this code
+    return thirdToLastLetterOfLastName;
+    }
+    console.log(main());
+    module.exports = main;
